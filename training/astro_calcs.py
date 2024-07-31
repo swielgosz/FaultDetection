3cfce9bb0462e25c54abbbdcd0317dbab3a6f47d
swielgosz <swielgosz20@gmail.com>
2024-07-31 13:57:42 -0400

    Documentation improvements

--- training/astro_calcs.py
+++ training/astro_calcs.py
@@ -1,6 +1,3 @@
-# Sarah Wielgosz
-# MLDS Onboarding
-
 import numpy as np
 import math
 import time
@@ -10,9 +7,9 @@ import matplotlib.gridspec as gridspec
 from scipy.signal import find_peaks
 
 # Specific Gravitation Constants
-MU_EARTH = 3.986004418 * 10 ** 14
+MU_EARTH = 3.986004418 * 10 ** 5 # km^3 s^-2
 G = 6.67408 * 10 ** -11 # m^3 kg^-1 s^-2
-RADIUS_EARTH = 6378000 # m
+RADIUS_EARTH = 6378 # km
 
 # Natural Basis
 DIR_X = np.array([1, 0, 0])
@@ -23,54 +20,58 @@ def cartesian_to_standard(r, v, mu=MU_EARTH):
     """Converts Cartian orbital elements to standard orbital elements.
 
     Args:
-        r (ndarray): Position vector (in meters)
-        v (ndarray): Velocity vector (in meters)
-        mu (float): Specific gravity of celestial body (in m^3 s^-2)
+        r (array[float]): Position vector (m)
+        v (array[float]): Velocity vector (m)
+        mu (float): Specific gravity of celestial body (m^3 s^-2)
 
     Returns:
         A list containing the orbital elements: [a, e, i, omega, w, u]
     """
+
+    # Convert initial state to float
     r = r.astype('float64')
     v = v.astype('float64')
 
-    pos = np.linalg.norm(r)
-    vel = np.linalg.norm(v)
-
-    _momentum = np.cross(r, v)
-    _line_of_nodes = np.cross(DIR_Z, _momentum)
-    _eccentricity = 1 / mu * ((vel ** 2 - mu / pos) * r - (np.dot(r, v)) * v)
-
-    e = np.linalg.norm(_eccentricity)
+    # Eccentricity, e
+    h = np.cross(r, v) # angular momentum
+    n = np.cross(DIR_Z, h) # line of nodes
+    e_vec = 1 / mu * ((np.linalg.norm(v) ** 2 - mu / np.linalg.norm(r)) * r - (np.dot(r, v)) * v) # points of periapsis
+    e = np.linalg.norm(e_vec) # eccentricity
     
-    energy = vel ** 2 / 2 - mu / pos
+    # Semi-major axis, a
+    energy = np.linalg.norm(v) ** 2 / 2 - mu / np.linalg.norm(r)
     a = - mu / (2 * energy)
 
-    cos_i = np.dot(_momentum, DIR_Z) / np.linalg.norm(_momentum)
+    # Angle of inclination, i
+    cos_i = np.dot(h, DIR_Z) / np.linalg.norm(h)
     i = np.degrees(np.arccos(cos_i))
 
-    cos_omega = np.dot(_line_of_nodes, DIR_X) / np.linalg.norm(_line_of_nodes)
+    # Right ascension of ascending node, omega (deg)
+    cos_omega = np.dot(n, DIR_X) / np.linalg.norm(n)
     omega = np.degrees(np.arccos(cos_omega))
 
-    if np.dot(_line_of_nodes, DIR_Y) < 0:
+    if np.dot(n, DIR_Y) < 0:
         omega = 360 - omega
 
-    cos_w = np.dot(_line_of_nodes, _eccentricity) / (np.linalg.norm(_line_of_nodes) * e)
+    # Argument of periapsis, w
+    cos_w = np.dot(n, e_vec) / (np.linalg.norm(n) * e)
     w = np.degrees(np.arccos(cos_w))
 
-    if np.dot(_eccentricity, DIR_Z) < 0:
+    if np.dot(e_vec, DIR_Z) < 0:
         w = 360 - w
     
-    cos_u = np.dot(_eccentricity, r) / (e * pos)
-    u = np.degrees(np.arccos(cos_u))
+    # Trua anomaly, nu
+    cos_nu = np.dot(e_vec, r) / (e * np.linalg.norm(r))
+    nu = np.degrees(np.arccos(cos_nu))
 
     if np.dot(r, v) < 0:
-        u = 360 - u
+        nu = 360 - nu
     
-    return [a, e, i, omega, w, u]
+    return [a, e, i, omega, w, nu]
 
 
-def standard_to_cartesian(a, e, i, raan, w, u, mu=MU_EARTH):
-    """_summary_
+def standard_to_cartesian(a, e, i, raan, w, nu, mu=MU_EARTH):
+    """Converts orbital elements to Cartesian coordinates
 
     Args:
         a (float): Semi-major axis (m)
@@ -78,18 +79,19 @@ def standard_to_cartesian(a, e, i, raan, w, u, mu=MU_EARTH):
         i (float): Inclination (deg)
         raan (float): RAAN (deg)
         w (float): Argument of periapsis (deg)
-        u (float): True anamoly (deg)
+        nu (float): True anamoly (deg)
 
     Returns:
         r
         v
     """
     
-    r = a * (1 - e ** 2) / (1 + e * np.cos(np.radians(u)))
+    # Position and velocity in perifocal frame
+    p = a * (1 - e ** 2) # periapsis
+    r_vec = np.array([p * np.cos(np.radians(nu)) / (1 + e * np.cos(np.radians(nu))), p * np.sin(np.radians(nu)) / (1 + e * np.cos(np.radians(nu))), 0])
+    v_vec = math.sqrt(mu / (a * (1 - e ** 2))) * np.array([-np.sin(np.radians(nu)), e + np.cos(np.radians(nu)), 0])
 
-    _r = np.array([r * np.cos(np.radians(u)), r * np.sin(np.radians(u)), 0])
-    _v = math.sqrt(mu / (a * (1 - e ** 2))) * np.array([-np.sin(np.radians(u)), e + np.cos(np.radians(u)), 0])
-
+    # Rotations
     rad_raan = np.radians(raan)
     rad_i = np.radians(i)
     rad_w = np.radians(w)
@@ -109,219 +111,54 @@ def standard_to_cartesian(a, e, i, raan, w, u, mu=MU_EARTH):
     R_eff = R_3_w @ R_1_i @ R_3_raan
     R_eff = np.matrix.transpose(R_eff)
     
-    _pos = R_eff @ _r
-    _vel = R_eff @ _v
-    return [_pos, _vel]
+    # Apply transformation to return to inertial frame
+    r = R_eff @ r_vec
+    v = R_eff @ v_vec
+    return [r, v]
 
 
-def propogate_2BP(t, state, mu=MU_EARTH):
-    """State space representation of Newton's Law of Gravitation. Only implemented for Earth.
+def propagate_2BP(t, X, mu=MU_EARTH):
+    """State space representation of Newton's Law of Gravitation
         Selected state variables are [r_x, r_y, r_z, v_x, v_y, v_z]
 
     Args:
         t (float): Current time step
-        state (arr): State vector of form [r_x, r_y, r_z, v_x, v_y, v_z]
+        X (arr): State vector of form [r_x, r_y, r_z, v_x, v_y, v_z]
 
     Returns:
         Value of velocity and acceleration at the given time and state in form:
             [v_x, v_y, v_z, a_x, a_y, a_z]
     """
 
-    _r = state[0:3]
-    _v = state[3:]
+    # Extract position and velocity
+    r = X[0:3]
+    v = X[3:6]
 
-    # a = - mu / norm(r) ^ 3 * r
-    _a = - mu / (np.linalg.norm(_r) ** 3) * _r
+    # Calculate acceleration for new state vector
+    a = - mu / (np.linalg.norm(r) ** 3) * r
 
-    val = np.array([_v[0], _v[1], _v[2], _a[0], _a[1], _a[2]])
-    return val
+    dXdt = np.array([v[0], v[1], v[2], a[0], a[1], a[2]])
+    return dXdt
 
-
-def calculate_orbital_period(a, mu=MU_EARTH):
-    """Calculates orbital period of an orbit.
-
-    Args:
-        a (float): Semi-major axis of orbit
-        mu (float, optional): Gravitional constant for body. Defaults to MU_EARTH.
-
-    Returns:
-        float: Orbital peroid in seconds
-    """
-    return 2 * math.pi * math.sqrt(a ** 3 / mu )
-
-
-def calculate_orbital_energy(r, v, mu=MU_EARTH):
-    """Calculates orbital energy
-
-    Args:
-        r (float): Radius magnitude
-        v (float): Velocity magnitude
-    
-    Returns:
-        float: Energy of orbit
-    """
-    return v ** 2 / 2 - mu / r
-
-def calculate_angular_momentum(_r, _v):
-    """Calculates angular momentum
-
-    Args:
-        _r (arr): Position vector
-        _v (arr): Velocity vector
-
-    Returns:
-        arr: Angular Momentum
-    """
-    return np.cross(_r, _v)
-
-
-def calculate_true_anomoly(_e, _r, _v):
-    """Calculate true anamoly
-
-    Args:
-        _e (_type_): _description_
-        _r (_type_): Radius vector
-        _v (_type_): Velocity vectore
-
-    Returns:
-        _type_: _description_
-    """
-    
-    v = np.arccos(np.dot(_e, _r) / (np.linalg.norm(_e) * np.linalg.norm(_r)))
-
-    if np.dot(_r, _v) < 0:
-        v = 2*math.pi - v
-    
-    return v
-
-def calculate_final_state(_r,_v,tof):
+def calculate_orbit(r,v,tof):
     """Calculate final state of spacecraft
 
     Args:
-        _r (arr[float]): Initial position vector (m)
-        _v (arr[float]): Initial velocity vectory (m/s)
+        r (arr[float]): Initial position vector (m)
+        v (arr[float]): Initial velocity vectory (m/s)
         tof (float): Time of flight (s)
         
     Returns: 
-        _rv (arr[float]): Final state vector
+        final_state (arr[float]): Final state vector
     """
 
-    init_state = np.array([_r[0], _r[1], _r[2], _v[0], _v[1], _v[2]])
+    init_state = np.array([r[0], r[1], r[2], v[0], v[1], v[2]])
     tol = 10**-13
 
-    orbit = solve_ivp(propogate_2BP, [0, tof], init_state, method="RK45", atol=tol, rtol=tol)
+    sol = solve_ivp(propagate_2BP, [0, tof], init_state, method="RK45", atol=tol, rtol=tol, t_eval = np.linspace(0, tof, 1000))
 
-    # pos = []
-    # vel = []
-    # acc = []
-    # for i in range(orbit.t.shape[0]):
-    #     pos.append(math.sqrt(orbit.y[0][i] ** 2 + orbit.y[1][i] ** 2 + orbit.y[2][i] ** 2))
-    #     vel.append(math.sqrt(orbit.y[3][i] ** 2 + orbit.y[4][i] ** 2 + orbit.y[5][i] ** 2))
-    #     a_x = - MU_EARTH / (pos[-1] ** 3) * orbit.y[0][i]
-    #     a_y = - MU_EARTH / (pos[-1] ** 3) * orbit.y[1][i]
-    #     a_z = - MU_EARTH / (pos[-1] ** 3) * orbit.y[2][i]
-    #     acc.append(math.sqrt(a_x ** 2 + a_y ** 2 + a_z ** 2))
+    # Convert Cartesian coordinates to orbital elements for each timestep
+    orbital_elements = np.array([cartesian_to_standard(sol.y[0:3, i], sol.y[3:6, i]) for i in range(len(sol.t))])
 
-    # #plt.style.use('_mpl-gallery')
-    # fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
-
-    # ax1.plot(orbit.t, pos)
-    # ax1.set_xlabel('Time (s)', size=10)
-    # ax1.set_ylabel('Position (m)', size=10)
-    # ax1.set_title("Postion Vs Time of Orbit A (t = 2P)")
-
-    # ax2.plot(orbit.t, vel)
-    # ax2.set_xlabel('Time (s)', size=10)
-    # ax2.set_ylabel('Velocity (m/s)', size=10)
-    # ax2.set_title("Velocity Vs Time of Orbit A (t = 2P)")
-
-
-    # ax3.plot(orbit.t, acc)
-    # ax3.set_xlabel('Time (s)', size=10)
-    # ax3.set_ylabel('Acceleration (m/s^2)', size=10)
-    # ax3.set_title("Acceleration Vs Time of Orbit A (t = 2P)")
-
-    # plt.tight_layout()
-
-    # fig = plt.figure()
-    # ax = plt.axes(projection='3d')
-    # ax.plot3D(orbit.y[0], orbit.y[1], orbit.y[2]) 
-    # ax.set_xlabel('x (m)', size=10)
-    # ax.set_ylabel('y (m)', size=10)
-    # ax.set_zlabel('z (m)', size=10)
-    # ax.set_title("Orbital Path of Orbit A")
-    
-    # plt.tight_layout()
-
-    # # label periapsis
-    # mins, _ = find_peaks(np.array(pos)*-1)
-
-    # min_inds = [0, *mins, len(pos) - 1]
-    # t_s = [orbit.t[i] for i in min_inds]
-    # y_s = [pos[i] for i in min_inds]
-
-    # ax1.plot(t_s, y_s, "ro")
-
-    # init_energy = vel[0] ** 2 / 2 - MU_EARTH / pos[0]
-    # energy_deviation = [calculate_orbital_energy(pos[i], vel[i]) - init_energy for i in range(len(orbit.t))]
-
-    # fig2 = plt.figure()
-    # ax4 = plt.axes()
-
-    # ax4.plot(orbit.t, energy_deviation)
-    # ax4.set_xlabel('Time (s)', size=10)
-    # ax4.set_ylabel('Deviation from Initial Orbital Energy (J/kg)', size=10)
-    # ax4.set_title("Deviation from Initial Orbital Energy Vs Time of Orbit A (t = 2P)")
-
-    # _rad = []
-    # _vel = []
-    # _angular_momentum = []
-    # for i in range(len(orbit.t)):
-    #     _rad_c = np.array([orbit.y[0][i], orbit.y[1][i], orbit.y[2][i]])
-    #     _vel_c = np.array([orbit.y[3][i], orbit.y[4][i], orbit.y[5][i]])
-    #     _rad.append(_rad_c)
-    #     _vel.append(_vel_c)
-    #     _angular_momentum.append(calculate_angular_momentum(_rad_c, _vel_c))
-
-    # init_momentum = np.linalg.norm(_angular_momentum[0])
-    # momentum_deviation = []
-    # for item in _angular_momentum:
-    #     momentum_deviation.append(np.linalg.norm(item) - init_momentum)
-
-    
-    # fig3, momentum_axes = plt.subplots(3, 1)
-
-    # for i, ax_symbol in enumerate(["x", "y", "z"]):
-    #     momentum_axes[i].plot(orbit.t, [j[i] for j in _angular_momentum])
-    #     momentum_axes[i].set_xlabel('Time (s)', size=10)
-    #     momentum_axes[i].set_ylabel(f'Angular Momentum (m^2/s)', size=10)
-    #     momentum_axes[i].set_title(f'{ax_symbol.upper()}-Component of Angular Momentum Vs Time')
-
-    # plt.tight_layout()
-    
-    # fig4 = plt.figure()
-    # ang_dev_axes = plt.axes()
-    # ang_dev_axes.plot(orbit.t, momentum_deviation)
-    
-    # ang_dev_axes.set_xlabel('Time (s)', size=10)
-    # ang_dev_axes.set_ylabel(f'Deviation of Angular Momentum (m^2/s)', size=10)
-    # ang_dev_axes.set_title('Deviation of Angular Momentum from Initial Value Vs Time')
-
-    # plt.tight_layout()
-
-    # true_anomoly = []
-    # for i in range(len(orbit.t)):
-    #     _e = 1 / MU_EARTH * ((vel[i] ** 2 - MU_EARTH / pos[i]) * _rad[i] - (np.dot(_rad[i], _vel[i])) * _vel[i])
-    #     true_anomoly.append(calculate_true_anomoly(_e, _rad[i], _vel[i]))
-
-    # fig5 = plt.figure()
-    # t_anom_axes = plt.axes()
-
-    # t_anom_axes.plot(orbit.t, true_anomoly)
-    # t_anom_axes.set_xlabel('Time (s)', size=10)
-    # t_anom_axes.set_ylabel(f'True Anomoly (rads)', size=10)
-    # t_anom_axes.set_title('True Anomoly Vs Time')
-
-    # plt.show()
-    final_state = [orbit.y[0][-1], orbit.y[1][-1], orbit.y[2][-1], orbit.y[3][-1], orbit.y[4][-1], orbit.y[5][-1]]
-    return final_state
+    # final_state = [orbit.y[0][-1], orbit.y[1][-1], orbit.y[2][-1], orbit.y[3][-1], orbit.y[4][-1], orbit.y[5][-1]]
+    return sol, orbital_elements
--- training/trajectory_gen.py
+++ training/trajectory_gen.py
@@ -11,7 +11,6 @@ from scipy.signal import find_peaks
 import csv
 
 def generate_dataset(output_path, _r_range, _v_range, tof_range, num_trajs):
-# def generate_dataset(output_path):
 
     dataset = []
 
@@ -19,15 +18,22 @@ def generate_dataset(output_path, _r_range, _v_range, tof_range, num_trajs):
         _r_gen = np.array([utils.signed_rand_float(_r_range), utils.signed_rand_float(_r_range), utils.signed_rand_float(_r_range)])
         _v_gen = np.array([utils.signed_rand_float(_v_range), utils.signed_rand_float(_v_range), utils.signed_rand_float(_v_range)])
         tof = random.uniform(*tof_range)
+        # tof = 48*60*60 # 48 hrs
 
-        state = calculate_final_state(_r_gen, _v_gen, tof)
-        dataset.append([*_r_gen.tolist(), *_v_gen.tolist(), tof, *state])
+        cartesian_sol, orbital_elements = calculate_orbit(_r_gen, _v_gen, tof)
+        
+        # Record time of flight, time, orbital elements
+        for t, elems in zip(cartesian_sol.t, orbital_elements):
+            dataset.append([tof, t, *elems])
+
+        # dataset.append([*_r_gen.tolist(), *_v_gen.tolist(), tof, *state])
 
         print(i/num_trajs * 100, end="\r")
 
     with open(output_path, 'w+', newline='') as f:
         writer = csv.writer(f)
+        writer.writerow(['time', 'a', 'e', 'i', 'omega', 'w', 'u'])
         writer.writerows(dataset)
 
 if __name__ == "__main__":
-    generate_dataset("../datasets/dataset.csv", [RADIUS_EARTH + 100 * 10 ** 3, 50 * 10 ** 6], [3 * 10 **3, 10 * 10 ** 3], [10*60, 2*60*60], 1 * 10 ** 5)
+    generate_dataset("../datasets/dataset.csv", [RADIUS_EARTH + 100, 10 * 10 ** 3], [3 , 20], [10*60, 2*60*60], 1 * 10 ** 2)
