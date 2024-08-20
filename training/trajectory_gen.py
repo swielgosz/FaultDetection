cd1b68d4c504332bbfc97d5eeb4555d8923a0dd4
swielgosz <swielgosz20@gmail.com>
2024-08-20 13:09:58 -0400

    Updated based on new astro package formatting

--- training/trajectory_gen.py
+++ training/trajectory_gen.py
@@ -1,13 +1,6 @@
-from astro_calcs import *
-from scipy.integrate import solve_ivp
-import utils
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
+from astro import astro_calcs, utils, constants, coordinate_conversions, utils
 import numpy as np
-import time
-import math
 import random
-from scipy.signal import find_peaks
 import csv
 
 def generate_dataset(output_path, _r_range, _v_range, tof_range, num_trajs):
@@ -20,7 +13,7 @@ def generate_dataset(output_path, _r_range, _v_range, tof_range, num_trajs):
         tof = random.uniform(*tof_range)
         # tof = 48*60*60 # 48 hrs
 
-        cartesian_sol, orbital_elements = calculate_orbit(_r_gen, _v_gen, tof)
+        cartesian_sol, orbital_elements = astro_calcs.calculate_orbit(_r_gen, _v_gen, tof)
         
         # Record time of flight, time, orbital elements
         for t, elems in zip(cartesian_sol.t, orbital_elements):
@@ -36,4 +29,4 @@ def generate_dataset(output_path, _r_range, _v_range, tof_range, num_trajs):
         writer.writerows(dataset)
 
 if __name__ == "__main__":
-    generate_dataset("../datasets/dataset.csv", [RADIUS_EARTH + 100, 10 * 10 ** 3], [3 , 20], [10*60, 2*60*60], 1 * 10 ** 2)
+    generate_dataset("../datasets/dataset.csv", [constants.RADIUS_EARTH + 100, 10 * 10 ** 3], [3 , 20], [10*60, 2*60*60], 1 * 10 ** 2)
