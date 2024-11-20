663c3408a050477e136afd8bdad49a38b636f6c8
swielgosz <sewielgo@umd.edu>
2024-11-20 14:28:55 -0500

    Main entry point. Training loop contained here.

--- neural_network/neural_ode.py
+++ neural_network/neural_ode.py
@@ -123,3 +123,76 @@ train_features = feature_scaler.fit_transform(train_features)
 # test_features = feature_scaler.transform(test_features)
 ts = train_features  # Time feature
 ys = train_labels  # Target (sin_nu and cos_nu)
+
+
+def main(
+    batch_size=1,
+    lr_strategy=(3e-3, 3e-3),
+    steps_strategy=(500, 500),
+    length_strategy=(0.1, 1),
+    width_size=64,
+    depth=2,
+    seed=5678,
+    plot=True,
+    print_every=100,
+):
+    key = jr.PRNGKey(seed)
+    data_key, model_key, loader_key = jr.split(key, 3)
+
+    # Use prepared data
+    ts = train_features  # Time feature
+    ts = ts.reshape(-1)
+    ys = train_labels  # Target (sin_nu and cos_nu)
+    ys = ys[np.newaxis, :, :]
+    _, length_size, data_size = ys.shape
+
+    model = NeuralODE(data_size, width_size, depth, key=model_key)
+
+    # Training loop like normal.
+    #
+    # Only thing to notice is that up until step 500 we train on only the first 10% of
+    # each time series. This is a standard trick to avoid getting caught in a local
+    # minimum.
+
+    @eqx.filter_value_and_grad
+    def grad_loss(model, ti, yi):
+        y_pred = jax.vmap(model, in_axes=(None, 0))(ti, yi[:, 0])
+        return jnp.mean((yi - y_pred) ** 2)
+
+    @eqx.filter_jit
+    def make_step(ti, yi, model, opt_state):
+        loss, grads = grad_loss(model, ti, yi)
+        updates, opt_state = optim.update(grads, opt_state)
+        model = eqx.apply_updates(model, updates)
+        return loss, model, opt_state
+
+    for lr, steps, length in zip(lr_strategy, steps_strategy, length_strategy):
+        optim = optax.adabelief(lr)
+        opt_state = optim.init(eqx.filter(model, eqx.is_inexact_array))
+        _ts = ts[: int(length_size * length)]
+        _ys = ys[:, : int(length_size * length)]
+        for step, (yi,) in zip(
+            range(steps), dataloader((_ys,), batch_size, key=loader_key)
+        ):
+            print(f"Step: {step + 1}/{steps}")
+            start = time.time()
+            loss, model, opt_state = make_step(_ts, yi, model, opt_state)
+            end = time.time()
+            if (step % print_every) == 0 or step == steps - 1:
+                print(f"Step: {step}, Loss: {loss}, Computation time: {end - start}")
+
+    if plot:
+        plt.plot(ts, ys[0, :, 0], c="dodgerblue", label="Real")
+        plt.plot(ts, ys[0, :, 1], c="dodgerblue")
+        model_y = model(ts, ys[0, 0])
+        plt.plot(ts, model_y[:, 0], c="crimson", label="Model")
+        plt.plot(ts, model_y[:, 1], c="crimson")
+        plt.legend()
+        plt.tight_layout()
+        plt.savefig("neural_ode.png")
+        plt.show()
+
+    return ts, ys, model
+
+
+ts, ys, model = main()
