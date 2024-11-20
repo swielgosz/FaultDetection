7b723518dd7f86f658e991569602965e0d2e42a3
swielgosz <sewielgo@umd.edu>
2024-11-20 14:27:11 -0500

    Define the function to be integrated.
    A neural ODE is defined as y(t) = y(t) + \int_{0}^{t} f_\theta(s, y(s)) ds. We define f_theta here.

--- neural_network/neural_ode.py
+++ neural_network/neural_ode.py
@@ -17,3 +17,21 @@ import matplotlib.pyplot as plt
 import optax  # https://github.com/deepmind/optax
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
+
+
+class Func(eqx.Module):
+    mlp: eqx.nn.MLP
+
+    def __init__(self, data_size, width_size, depth, *, key, **kwargs):
+        super().__init__(**kwargs)
+        self.mlp = eqx.nn.MLP(
+            in_size=data_size,
+            out_size=data_size,
+            width_size=width_size,
+            depth=depth,
+            activation=jnn.softplus,
+            key=key,
+        )
+
+    def __call__(self, t, y, args):
+        return self.mlp(y)
