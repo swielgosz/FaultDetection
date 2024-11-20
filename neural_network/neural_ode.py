aa6665ee78037833111d331950a4812e48f17e72
swielgosz <sewielgo@umd.edu>
2024-11-20 14:27:41 -0500

    Wrap the ODE solve.

--- neural_network/neural_ode.py
+++ neural_network/neural_ode.py
@@ -35,3 +35,24 @@ class Func(eqx.Module):
 
     def __call__(self, t, y, args):
         return self.mlp(y)
+
+
+class NeuralODE(eqx.Module):
+    func: Func
+
+    def __init__(self, data_size, width_size, depth, *, key, **kwargs):
+        super().__init__(**kwargs)
+        self.func = Func(data_size, width_size, depth, key=key)
+
+    def __call__(self, ts, y0):
+        solution = diffrax.diffeqsolve(
+            diffrax.ODETerm(self.func),
+            diffrax.Tsit5(),
+            t0=ts[0],
+            t1=ts[-1],
+            dt0=ts[1] - ts[0],
+            y0=y0,
+            stepsize_controller=diffrax.PIDController(rtol=1e-3, atol=1e-6),
+            saveat=diffrax.SaveAt(ts=ts),
+        )
+        return solution.ys
